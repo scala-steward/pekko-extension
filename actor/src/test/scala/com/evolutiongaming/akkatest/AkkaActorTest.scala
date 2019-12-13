cd79ee7df2b103cf6c6aa2604f31ac18d31c8f3d
Yaroslav Klymko <t3hnar@gmail.com>
2019-12-13 16:50:07 +0100

    improve tests

--- actor/src/test/scala/com/evolutiongaming/akkatest/AkkaActorTest.scala
+++ actor/src/test/scala/com/evolutiongaming/akkatest/AkkaActorTest.scala
@@ -9,13 +9,11 @@ import scala.util.{Failure, Success, Try}
 class AkkaActorTest extends AsyncFunSuite with Matchers {
 
   test("akka modules are not of same version") {
-    for {
-      error <- `akka modules are of same version`.transform((a: Try[Assertion]) => Success(a))
-    } yield {
-      error match {
+    `akka modules are of same version`
+      .transform((a: Try[Assertion]) => Success(a))
+      .map {
         case Failure(a: IllegalStateException) if a.getMessage contains "akka-slf4j" => succeed
         case _                                                                       => fail()
       }
-    }
   }
 }
--- http/src/test/scala/com/evolutiongaming/akkatest/AkkaHttpTest.scala
+++ http/src/test/scala/com/evolutiongaming/akkatest/AkkaHttpTest.scala
@@ -8,14 +8,11 @@ class AkkaHttpTest extends AsyncFunSuite with Matchers {
   import AkkaHttpSuite._
 
   test("akka-http modules are not of same version") {
-    for {
-      error <- `akka-http modules are of same version`.transform((a: Try[Assertion]) => Success(a))
-    } yield {
-      //      error should matchPattern { case Failure(_: IllegalStateException) => }
-      error match {
+    `akka-http modules are of same version`
+      .transform((a: Try[Assertion]) => Success(a))
+      .map {
         case Failure(a: IllegalStateException) if a.getMessage contains "akka-http-testkit" => succeed
         case _                                                                              => fail()
       }
-    }
   }
 }
