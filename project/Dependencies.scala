cbf07df43211acb704af8ea7db6aa5bfc61f2e66
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-08-14 17:40:01 +0200

    Update pekko-http-testkit to 1.2.0 (#8)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
     val Core = "org.apache.pekko" %% "pekko-http-core" % version
     val Testkit = "org.apache.pekko" %% "pekko-http-testkit" % version
 
-    val OlderTestkit = "org.apache.pekko" %% "pekko-http-testkit" % "1.1.0"
+    val OlderTestkit = "org.apache.pekko" %% "pekko-http-testkit" % "1.2.0"
   }
 
   object Cats {
